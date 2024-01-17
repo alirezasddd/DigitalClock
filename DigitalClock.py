6951fc0312a92d15f60afca14a51240a01212f6d
Alireza <alireza.sad.ger@gmail.com>
2024-01-18 02:19:50 +0330

    zone added

--- DigitalClock.py
+++ DigitalClock.py
@@ -3,7 +3,7 @@ import time
 
 window = Tk()
 window.title("digital clock")
-window.geometry("600x150")
+window.geometry("600x170")
 
 
 def clock():
@@ -11,14 +11,20 @@ def clock():
     minute = time.strftime("%M")
     second = time.strftime("%S")
     am_pm = time.strftime("%p")
+    zone = time.strftime("%Z")
+
     mytext = hour + ":" + minute + ":" + second + "  " + am_pm
+    mytext2 = zone
     mylabel.config(text=mytext)
+    mylabel2.config(text=mytext2)
     mylabel.after(1000, clock)
 
 
 mylabel = Label(window, text="", font=(
     'arial', 72), fg="silver", bg="black")
 mylabel.pack()
+mylabel2 = Label(window, text="", font=("Arial", 24))
+mylabel2.pack()
 
 clock()
 window.mainloop()
